dc76750eba7eb65ba68f609259fd33d8195e477b
figo hu <okjjhfj111@126.com>
2019-10-15 21:51:11 +0800

    修改众包送物品动画

--- src/main/resources/static/editor-app/stencil-controller.js
+++ src/main/resources/static/editor-app/stencil-controller.js
@@ -487,6 +487,7 @@ angular.module('activitiModeler')
                 var inputProp = $scope.getPropertybyKey(propertylist, "oryx-input");
                 var outputProp = $scope.getPropertybyKey(propertylist, "oryx-output");
                 var AEProp = $scope.getPropertybyKey(propertylist, "oryx-activityelement");
+                var direction = $scope.getPropertybyKey(propertylist, "oryx-animate_direction");
 
                 // create jquery selector
                 var inputPropSel = jQuery("#" + inputProp.id).parent().parent();
@@ -526,41 +527,60 @@ angular.module('activitiModeler')
 
                     }
                 } else {
-                    // 众包
-                    // 0.订单输入？,1.人闪两下,2.人前往目标位置，3.人取东西；4.人携带东西回到原来位置
-                    // AE: 人；    input：指令；    output： 取的东西
-                    // step0
-                    $scope.playAnimation(inputPropSel, "linear", "0", pos_AE, pos_input);
-                    $scope.stopAnimation(inputPropSel, 1500);
-                    // step1
-                    $scope.playAnimation(AEPropSel, "flash", "0", pos_AE, pos_AE);
-                    $scope.stopAnimation(AEPropSel, 1500);
-                    // step2
-                    setTimeout(function () {
-                        $scope.playAnimation(AEPropSel, "linear2", "0", pos_output, pos_AE);
+                    if(direction === "0"){
+                        // 众包取东西
+                        // 0.订单输入？,1.人闪两下,2.人前往目标位置，3.人取东西；4.人携带东西回到原来位置
+                        // AE: 人；    input：指令；    output： 取的东西
+                        // step0
+                        $scope.playAnimation(inputPropSel, "linear", "0", pos_AE, pos_input);
+                        $scope.stopAnimation(inputPropSel, 1500);
+                        // step1
+                        $scope.playAnimation(AEPropSel, "flash", "0", pos_AE, pos_AE);
                         $scope.stopAnimation(AEPropSel, 1500);
-                    }, 2000);
+                        // step2
+                        setTimeout(function () {
+                            $scope.playAnimation(AEPropSel, "linear2", "0", pos_output, pos_AE);
+                            $scope.stopAnimation(AEPropSel, 1500);
+                        }, 2000);
 
-                    // step3
-                    setTimeout(function () {
-                        $scope.playAnimation(outputPropSel, "flash", "0", pos_output, pos_output);
-                        $scope.stopAnimation(outputPropSel, 1500);
-                    }, 3000);
+                        // step3
+                        setTimeout(function () {
+                            $scope.playAnimation(outputPropSel, "flash", "0", pos_output, pos_output);
+                            $scope.stopAnimation(outputPropSel, 1500);
+                        }, 3000);
 
-                    // step4
-                    setTimeout(function () {
-                        var obj_pos_output = {x: pos_output.x, y: pos_output.y};
-                        var obj_pos_AE = {x: pos_AE.x, y: pos_AE.y};
-                        if (pos_output.x - 40 > 0) {
-                            obj_pos_output.x -= 40;
-                            obj_pos_AE.x -= 40;
-                        }
-                        $scope.playAnimation(AEPropSel, "linear", "1", pos_output, pos_AE);
-                        $scope.playAnimation(outputPropSel, "linear", "1", obj_pos_output, obj_pos_AE);
+                        // step4
+                        setTimeout(function () {
+                            var obj_pos_output = {x: pos_output.x, y: pos_output.y};
+                            var obj_pos_AE = {x: pos_AE.x, y: pos_AE.y};
+                            if (pos_output.x - 40 > 0) {
+                                obj_pos_output.x -= 40;
+                                obj_pos_AE.x -= 40;
+                            }
+                            $scope.playAnimation(AEPropSel, "linear", "1", pos_output, pos_AE);
+                            $scope.playAnimation(outputPropSel, "linear", "1", obj_pos_output, obj_pos_AE);
 
-                        $scope.stopAnimation(AEPropSel, 2000);
-                        $scope.stopAnimation(outputPropSel, 2000);
-                    }, 5000);
+                            $scope.stopAnimation(AEPropSel, 2000);
+                            $scope.stopAnimation(outputPropSel, 2000);
+                        }, 5500);
+                    }else{
+                        // 众包送东西
+                        // 1. 人闪两下；2.人携带东西到目标位置
+                        // step1
+                        setTimeout(function () {
+                            $scope.playAnimation(AEPropSel, "flash", "0", pos_AE, pos_AE);
+                            $scope.stopAnimation(AEPropSel, 1500);
+                        }, playTime);
+                        playTime += 2000;
+
+                        // step2
+                        setTimeout(function () {
+                            $scope.playAnimation(AEPropSel, "linear2", "0", pos_output, pos_AE);
+                            $scope.stopAnimation(AEPropSel, 1500);
+                        }, playTime);
+
+                    }
+
 
                 }
 
