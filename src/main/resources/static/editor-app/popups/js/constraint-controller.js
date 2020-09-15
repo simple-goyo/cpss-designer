d115cc969d1467b2a492797729999a8a7be9a550
Figo Hu <okjjhfj111@126.com>
2020-09-16 01:47:51 +0800

    修改约束视图部分样式

--- src/main/resources/static/editor-app/popups/js/constraint-controller.js
+++ src/main/resources/static/editor-app/popups/js/constraint-controller.js
@@ -1,11 +1,27 @@
 var ConstraintController = ['$scope','$rootScope', function ($scope,$rootScope) {
+    $scope.entities = [];
+
+    $scope.entities.push({id:"0",name:"11",description:"aa",icon:"socialentity/person.png"});
+
     $scope.init = function (){
-        var parent = jQuery('.constraintViewer');
-        var div = ORYX.Editor.graft("http://www.w3.org/1999/xhtml", parent, ['div']);
-        div.addClassName("HAHAHAHAHA");
-    }
+        // var parent = jQuery('.constraintViewer');
+        // var div = ORYX.Editor.graft("http://www.w3.org/1999/xhtml", parent, ['div']);
+        // div.addClassName("HAHAHAHAHA");
+        var command = new VIEWER.CreateCanvas();
+        $scope.editor.executeCommands([command]);
+    };
 
     $scope.close = function (){
         $scope.$hide();
     }
 }];
+
+var VIEWER = VIEWER || {};
+
+VIEWER.CreateCanvas = ORYX.Editor.extend({
+    construct: function (){
+        let v = this.getStencilSets().values();
+        console.log(v);
+        // this._createCanvas(null, null);
+    }
+})
