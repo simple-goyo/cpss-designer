f486e532898caac3d5f16664680ef4973c6ae568
lxm <229078506@qq.com>
2020-10-20 01:33:19 +0800

    增加基于字段的动态绑定设置

--- src/main/resources/static/editor-app/popups/js/entity-specific-properties-controller.js
+++ src/main/resources/static/editor-app/popups/js/entity-specific-properties-controller.js
@@ -1,7 +1,7 @@
 var EntitySpecificPropertiesController = ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
     let selectedShape = $scope.editor.getSelection()[0];
     $scope.resName = selectedShape.properties["oryx-type"] === "房间" ? "Room" : selectedShape.properties['oryx-name'];
-    $scope.rules = ["靠近", "远离"];
+    $scope.rules = ["靠近", "远离", "大于", "小于", "等于"];
     $scope.getRelatedEntities = function () {
         let relatedEntities = [];
         let sceneId = $rootScope.scenes[$rootScope.selectedSceneIndex].id;
@@ -39,6 +39,16 @@ var EntitySpecificPropertiesController = ['$scope', '$rootScope', '$http', funct
         return relatedEntities;
     }
 
+    $scope.getData = function () {
+        let sceneId = $rootScope.scenes[$rootScope.selectedSceneIndex].id;
+        let traceableScenes = $scope.getTraceableScenes(sceneId);
+        let action = $scope.getHighlightedShape();
+        let traceableActions = []
+        if (action !== undefined)
+            traceableActions = $scope.getTraceableActions(action);
+        return $scope.getVisibleParameters(sceneId, traceableScenes, traceableActions);
+    }
+
     $scope.isEntity = function (stencilId) {
         return stencilId === "Person" || stencilId === "Person_Worker" || stencilId === "Group" || stencilId === "Organization"
             || stencilId === "PysicalObject" || stencilId === "Robot" || stencilId === "Device"
@@ -47,6 +57,7 @@ var EntitySpecificPropertiesController = ['$scope', '$rootScope', '$http', funct
     }
 
     $scope.entities = $scope.getRelatedEntities();
+    $scope.data = $scope.getData();
     let olderProperties = selectedShape.properties['oryx-entityspecificproperties'];
     if (olderProperties !== "" && olderProperties.bindDynamically !== undefined) {
         $scope.bindDynamically = olderProperties.bindDynamically;
@@ -61,7 +72,7 @@ var EntitySpecificPropertiesController = ['$scope', '$rootScope', '$http', funct
         if (data.properties !== undefined && data.properties.length > 0) {
             data.properties.forEach((property) => {
                 if (olderProperties === "" || olderProperties.properties === undefined || olderProperties.properties[property] === undefined) {
-                    $scope.properties[property] = {text: "", entity: {name: "", id: ""}, rule: ""};
+                    $scope.properties[property] = {type: "entity", data: "", text: "", entity: "", rule: ""};
                 } else
                     $scope.properties[property] = olderProperties.properties[property];
             });
@@ -73,36 +84,12 @@ var EntitySpecificPropertiesController = ['$scope', '$rootScope', '$http', funct
     $scope.save = function () {
         let entitySpecificProperties = {};
         entitySpecificProperties.bindDynamically = $scope.bindDynamically;
-        if ($scope.bindDynamically !== true) {
-            for (let property in $scope.properties) {
-                let text = document.getElementById(property).value;
-                $scope.properties[property] = {text: text, entity: {name: "", id: ""}, rule: ""};
-            }
-        } else {
-            for (let property in $scope.properties) {
-                let entityId = property + "_target_entity";
-                let ruleId = property + "_rule";
-                let entity = document.getElementById(entityId).value;
-                let rule = document.getElementById(ruleId).value;
-                if (entity === "") {
-                    entity = {name: "", id: ""};
-                } else {
-                    entity = JSON.parse(entity);
-                }
-                $scope.properties[property] = {text: "", entity: entity, rule: rule};
+        for (let property in $scope.properties) {
+            if ($scope.properties[property].entity !== "") {
+                if ($scope.properties[property].entity.name === undefined)
+                    $scope.properties[property].entity = JSON.parse($scope.properties[property].entity);
             }
         }
-
-        // let entitySpecificProperties = {};
-        // entitySpecificProperties.bindDynamically = $scope.bindDynamically;
-        // for (let property in $scope.properties) {
-        //     if ($scope.properties[property].entity !== "") {
-        //         if ($scope.properties[property].entity.name === undefined)
-        //             $scope.properties[property].entity = JSON.parse($scope.properties[property].entity);
-        //
-        //     }
-        // }
-        // entitySpecificProperties.properties = $scope.properties;
         entitySpecificProperties.properties = $scope.properties;
         selectedShape.setProperty("oryx-entityspecificproperties", entitySpecificProperties);
         $scope.close();
