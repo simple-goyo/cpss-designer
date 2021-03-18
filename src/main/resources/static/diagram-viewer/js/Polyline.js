be1bc31079d8047872c0178fdaae5ce836df1bfe
LAPTOP-6JO0PLKH\FigoHu <okjjhfj111@126.com>
2021-03-18 18:37:58 +0800

    修改saveModel方法的默认参数传递方式，修改tomcat最大header值，保证模型能够正确传递

--- src/main/resources/static/diagram-viewer/js/Polyline.js
+++ src/main/resources/static/diagram-viewer/js/Polyline.js
@@ -126,7 +126,7 @@ Polyline.prototype = {
 	},
 	
 	getAnchors: function(){
-		// вернуть отсортированный массив
+		// РІРµСЂРЅСѓС‚СЊ РѕС‚СЃРѕСЂС‚РёСЂРѕРІР°РЅРЅС‹Р№ РјР°СЃСЃРёРІ
 		// ????
 		return this.anchors;
 	},
@@ -155,7 +155,7 @@ Polyline.prototype = {
 		} else if (!index) {
 			index = this.anchors.length;
 		} else {
-			// перебрать anchors, сдвинуть позицию для каждого, начиная с index
+			// РїРµСЂРµР±СЂР°С‚СЊ anchors, СЃРґРІРёРЅСѓС‚СЊ РїРѕР·РёС†РёСЋ РґР»СЏ РєР°Р¶РґРѕРіРѕ, РЅР°С‡РёРЅР°СЏ СЃ index
 			//var anchor = this.getAnchor()
 			for(var i=0; i < this.getAnchorsCount(); i++){
 				var anchor = this.anchors[i];
